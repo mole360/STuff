5b7c6f9d15e4a4c2194ee0a2232af3443dfc12a8
misko.olexa@gmail.com <misko.olexa@gmail.com>
2024-10-29 16:25:10 +0100

    sdrt

---
+++ src/Shape.java
@@ -0,0 +1,41 @@
+public interface Shape {
+    public void getAtributes();
+}
+
+class Circle implements Shape {
+    private double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    @Override
+    public void getAtributes() {
+        System.out.println("Circle: radius is " + radius + ".");
+        double area = Math.PI * radius * radius;
+        System.out.println("Circle: area is " + area + ".");
+    }
+}
+class Rectangle implements Shape {
+    private double width;
+    private double height;
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    @Override
+    public void getAtributes() {
+        System.out.println("Rectangle: width is " + width + ".");
+        System.out.println("Rectangle: height is " + height + ".");
+        System.out.println("Rectangle: area is " + width * height + ".");
+    }
+}
+class Square implements Shape {
+    private double width;
+    public Square(double width) {
+        this.width = width;
+    }
+    @Override
+    public void getAtributes() {
+        System.out.println("Square: width is " + width + ".");
+        System.out.println("Square: area is " + width * width + ".");
+    }
+}
